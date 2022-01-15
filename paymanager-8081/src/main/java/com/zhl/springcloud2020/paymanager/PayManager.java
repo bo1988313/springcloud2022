2c07cd57340683274ef2d5bede8523162ce9b99a
bo1988313 <707169146@qq.com>
2022-01-16 01:53:41 +0800

    Initial commit

---
+++ paymanager-8081/src/main/java/com/zhl/springcloud2020/paymanager/PayManager.java
@@ -0,0 +1,15 @@
+package com.zhl.springcloud2020.paymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author Administrator
+ */
+@SpringBootApplication
+public class PayManager {
+    public static void main(String[] args) {
+        SpringApplication.run(PayManager.class, args);
+    }
+}
