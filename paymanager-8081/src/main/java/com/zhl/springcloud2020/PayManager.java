fd079d37d16c5602ca33a525a5c93ee30c1cbbe9
bo1988313 <707169146@qq.com>
2022-01-16 23:33:24 +0800

    eureka服务端、客户端搭建

--- paymanager-8081/src/main/java/com/zhl/springcloud2020/PayManager.java
+++ paymanager-8081/src/main/java/com/zhl/springcloud2020/PayManager.java
@@ -3,12 +3,14 @@ package com.zhl.springcloud2020;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  *
  * @author Administrator
  */
 @SpringBootApplication
+@EnableEurekaClient
 @MapperScan("com.zhl.springcloud2020.paymanager.mapper")
 public class PayManager {
     public static void main(String[] args) {
