a549964cd80fc6797fe3cc9e4b7e93424a7bba47
bo1988313 <707169146@qq.com>
2022-01-20 00:52:09 +0800

    HystrixDashBoard  服务搭建

--- cloud-eureka-hystrix-pay/src/main/java/com/zhl/hystrixpay/HystrixPay8085.java
+++ cloud-eureka-hystrix-pay/src/main/java/com/zhl/hystrixpay/HystrixPay8085.java
@@ -1,9 +1,13 @@
 package com.zhl.hystrixpay;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author Administrator
@@ -11,8 +15,20 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableHystrix
+@EnableCircuitBreaker
 public class HystrixPay8085 {
     public static void main(String[] args) {
         SpringApplication.run(HystrixPay8085.class, args);
+    }
+
+
+    @Bean
+    public ServletRegistrationBean getServlet(){
+        HystrixMetricsStreamServlet hystrixMetricsStreamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(hystrixMetricsStreamServlet);
+        servletRegistrationBean.setLoadOnStartup(1);
+        servletRegistrationBean.addUrlMappings("/hystrix.stream");
+        servletRegistrationBean.setName("HystrixMetricsStreamServlet");
+        return servletRegistrationBean;
     }
 }
--- cloud.zhl.hystrixdashboard9001/src/main/java/com/zhl/hystrix/dashaboard/HystrixDashBoard.java
+++ cloud.zhl.hystrixdashboard9001/src/main/java/com/zhl/hystrix/dashaboard/HystrixDashBoard.java
@@ -1,23 +1,21 @@
-package com.zhl.comsumer;
+package com.zhl.hystrix.dashaboard;
 
 import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 
 /**
  * @author Administrator
  */
 @SpringBootApplication
-@EnableFeignClients
-//@EnableHystrix
-public class FeignHystrixOrder80 {
+@EnableHystrixDashboard
+public class HystrixDashBoard {
 
     public static void main(String[] args) {
-        SpringApplication.run(FeignHystrixOrder80.class, args);
+        SpringApplication.run(HystrixDashBoard.class, args);
     }
 
     @Bean
