155666df463a521751dedff5ba01a2e625607845
bo1988313 <707169146@qq.com>
2022-01-22 18:26:36 +0800

    config + bus + kafka 实现配置文件动态刷新 http://config服务端host:port/actuator/bus-refresh

--- cloud-eureka-hystrix-pay8086/src/main/java/com/zhl/hystrixpay8086/controller/PaymentController.java
+++ cloud-eureka-hystrix-pay8086/src/main/java/com/zhl/hystrixpay8086/controller/PaymentController.java
@@ -2,6 +2,8 @@ package com.zhl.hystrixpay8086.controller;
 
 import com.zhl.hystrixpay8086.service.HystrixPayServer;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +17,12 @@ import javax.annotation.Resource;
 @RestController
 @RequestMapping(value="/payment")
 @Slf4j
+@RefreshScope
 public class PaymentController {
+
+    @Value("${config.version}")
+    private String version;
+
     @Resource
     private HystrixPayServer hystrixPayServer;
 
@@ -33,6 +40,16 @@ public class PaymentController {
     public String circuitBreaker(@PathVariable("id") long id) {
         try {
             return hystrixPayServer.circuitBreaker(id);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    @GetMapping("/propertiesinfo")
+    public String circuitBreaker() {
+        try {
+            return version;
         } catch (Exception e) {
             e.printStackTrace();
             return "";
